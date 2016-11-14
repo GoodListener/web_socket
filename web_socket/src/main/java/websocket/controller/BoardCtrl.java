5da2f186f51c048e3538683dbb220e92f0ab88c0
응철 <>
2016-11-14 23:18:58 +0900

    1. 웹소켓 리포 추가

--- web_socket/src/main/java/websocket/controller/BoardCtrl.java
+++ web_socket/src/main/java/websocket/controller/BoardCtrl.java
@@ -1,5 +1,14 @@
 package websocket.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("BoardCtrl")
 public class BoardCtrl {
 
+  @RequestMapping("/pages/test")
+  public void test(String text) throws Exception {
+    System.out.println(text);
+  }
+  
 }
