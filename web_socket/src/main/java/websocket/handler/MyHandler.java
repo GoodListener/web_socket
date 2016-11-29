922057b46479938c997393a3be97493558ce9b48
응철 <>
2016-11-30 00:16:36 +0900

    2. 실시간 점찍기 가능

--- web_socket/src/main/java/websocket/handler/MyHandler.java
+++ web_socket/src/main/java/websocket/handler/MyHandler.java
@@ -1,5 +1,8 @@
 package websocket.handler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -7,18 +10,29 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class MyHandler extends TextWebSocketHandler {
   
+  List<WebSocketSession> webSocketSessionList = new ArrayList<>();
+  
   @Override
   protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-    TextMessage echoMessage = new TextMessage("ECHO :" + message.getPayload());
-    session.sendMessage(echoMessage);
+    System.out.println(message);
+    TextMessage echoMessage = new TextMessage(message.getPayload());
+    for (WebSocketSession webSocketSession : webSocketSessionList)
+    {
+      if (!session.equals(webSocketSession) && webSocketSession.isOpen())
+        webSocketSession.sendMessage(echoMessage);
+    }
   }
 
   @Override
   public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+    System.out.println(session);
+    webSocketSessionList.add(session);
   }
   
   @Override
   public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+    System.out.println(session);
+    System.out.println(status);
   }
   
 }
